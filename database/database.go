965d65d19581d349da5628d306a9f60d59e88710
zawkindev <zawkindev@gmail.com>
2024-08-31 17:00:39 +0500

    feat: database pkg

---
+++ database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+)
+
+var DB *sql.DB
+
+func Init() {
+	var err error
+	DB, err = sql.Open("postgres", "user=postgres password=m dbname=blog sslmode=required")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	createTable()
+}
+
+func createTable() {
+	query := `
+	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+	
+	CREATE TABLE IF NOT EXISTS posts(
+		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		title VARCHAR(255) NOT NULL,
+		content TEXT NOT NULL,
+		date VARCHAR(10) NOT NULL
+	);
+	`
+
+	_, err := DB.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
