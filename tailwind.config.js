9f61d66a7129d4353daf05da1c34c437f1f08354
zawkindev <zawkindev@gmail.com>
2024-08-31 16:35:20 +0500

    feat: template for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          50: "#252525",
+          100: "#2e2e2e",
+          300: "#1e1e1e",
+          600: "#111111",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
